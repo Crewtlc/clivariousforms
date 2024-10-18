fe51de6cef5b3c195e45f514482d75dbe09984ee
Crewtlc <longhourswaltair@gmail.com>
2024-10-18 17:10:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 const button = document.getElementById('interactiveButton');
 
 button.addEventListener('BA-Check Form', function() {
-  if (button.classList.contains('clicked')) {
-    button.classList.remove('clicked');
-    button.textContent = 'BA-Check Form';
-  } else {
-    button.classList.add('clicked');
-    button.textContent = 'Clicked!';
-    
+     
     // Add the link here to open a specific URL
     window.location.href = 'https://crewtlc.github.io/bacheck/'; // replace with your desired link
     // or use window.open('https://www.example.com', '_blank'); to open in a new tab
